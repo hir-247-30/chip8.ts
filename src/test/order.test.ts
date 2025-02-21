0d0af5baf066c944b2e933450e4b1b5f572c9e28
Hiroshi Hagiwara <mukakinkatsubou@gmail.com>
2025-02-21 13:04:56 +0900

    fix: ユニットテスト途中

--- src/test/order.test.ts
+++ src/test/order.test.ts
@@ -4,22 +4,136 @@ import { assert } from 'chai';
 import { describe, it } from 'mocha';
 
 describe('order', () => {
-    before(function() {
-    });
-    after(function() {
-    });
+    const cpu = new CPU();
 
-    it('addVxVy', () => {
+    before(function() {
         let romBuffer: Buffer<ArrayBufferLike>
         try {
             romBuffer = fs.readFileSync('rom/ibm_logo.ch8');
         } catch {
             throw new Error('romが読み込めない');
         }
-        
-        const cpu = new CPU();
         cpu.readRom(romBuffer);
+    });
 
+    after(function() {
+        console.log('COMPLETE!')
+    });
+
+    it('00E0 - CLS', () => {
+        // TODO
+        // 64 と 32 は別ファイルでconstとして定義する
+        cpu.displayBuffer = new Array(64 * 32).fill(1);
+        cpu._cls();
+        cpu.displayBuffer.forEach(pixel => {
+            assert.deepEqual(pixel[0], 0);
+        });
+    });
+
+    it('00EE - RET', () => {
+        cpu.stack[cpu.stackPointer] = 0x300;
+        cpu.programCounter = 0x200;
+        cpu._ret();
+        assert.deepEqual(cpu.programCounter, 0x300);
+    });
+    
+    it('1nnn - JP addr', () => {
+        cpu._jpAddr(0x1234);
+        assert.deepEqual(cpu.programCounter, 0x1234);
+    });
+
+    // なんか怪しいような
+    it('2nnn - CALL addr', () => {
+        cpu.programCounter = 0x200;
+        cpu._callAddr(0x0123);
+        assert.deepEqual(cpu.stack[cpu.stackPointer], 0x200);
+        assert.deepEqual(cpu.programCounter, 0x123);
+    });
+    
+    it('3xkk - SE Vx, byte', () => {
+        // v[x] == kk
+        cpu.registerV[0xA] = 0x45;
+        cpu.programCounter = 0x200;
+        cpu._seVxByte(0x000A, 0x0045);
+        assert.deepEqual(cpu.programCounter, 0x200 + 2);
+
+        // v[x] != kk
+        cpu.registerV[0xA] = 0x44;
+        cpu.programCounter = 0x200;
+        cpu._seVxByte(0x000A, 0x0045);
+        assert.deepEqual(cpu.programCounter, 0x200 + 0);
+    });
+    
+    it('4xkk - SNE Vx, byte', () => {
+        // v[x] != kk
+        cpu.registerV[0x1] = 0x10;
+        cpu.programCounter = 0x200;
+        cpu._sneVxByte(0x1, 0x12);
+        assert.deepEqual(cpu.programCounter, 0x200 + 2);
+
+        // v[x] == kk
+        cpu.registerV[0x1] = 0x12;
+        cpu.programCounter = 0x200;
+        cpu._sneVxByte(0x1, 0x12);
+        assert.deepEqual(cpu.programCounter, 0x200 + 0);
+    });
+    
+    it('5xy0 - SE Vx, Vy', () => {
+        // v[x] == v[y]
+        cpu.registerV[0x2] = 0x30;
+        cpu.registerV[0x3] = 0x30;
+        cpu.programCounter = 0x200;
+        cpu._seVxVy(0x2, 0x3);
+        assert.deepEqual(cpu.programCounter, 0x200 + 2);
+
+        // v[x] != v[y]
+        cpu.registerV[0x2] = 0x31;
+        cpu.registerV[0x3] = 0x30;
+        cpu.programCounter = 0x200;
+        cpu._seVxVy(0x2, 0x3);
+        assert.deepEqual(cpu.programCounter, 0x200 + 0);
+    });
+    
+    it('6xkk - LD Vx, byte', () => {
+        cpu._ldVxByte(0xB, 0xFF);
+        assert.deepEqual(cpu.registerV[0xB], 0xFF);
+    });
+    
+    it('7xkk - ADD Vx, byte', () => {
+        cpu.registerV[0x1] = 0x05;
+        cpu._addVxByte(0x1, 0x3);
+        assert.deepEqual(cpu.registerV[0x1], 0x08);
+    });
+    
+    it('8xy0 - LD Vx, Vy', () => {
+        cpu.registerV[0xC] = 0xAA;
+        cpu.registerV[0xD] = 0x55;
+        cpu._ldVxVy(0xC, 0xD);
+        assert.deepEqual(cpu.registerV[0xC], 0x55);
+    });
+    
+    it('8xy1 - OR Vx, Vy', () => {
+        cpu.registerV[0xE] = 0xF0;
+        cpu.registerV[0xF] = 0x0F;
+        cpu._orVxVy(0xE, 0xF);
+        assert.deepEqual(cpu.registerV[0xE], 0xFF);
+    });
+    
+    it('8xy2 - AND Vx, Vy', () => {
+        cpu.registerV[0x3] = 0xF0;
+        cpu.registerV[0x4] = 0x0F;
+        cpu._andVxVy(0x3, 0x4);
+        assert.deepEqual(cpu.registerV[0x3], 0x00);
+    });
+    
+    it('8xy3 - XOR Vx, Vy', () => {
+        cpu.registerV[0x2] = 0xFF;
+        cpu.registerV[0x1] = 0x0F;
+        cpu._xorVxVy(0x2, 0x1);
+        assert.deepEqual(cpu.registerV[0x2], 0xF0);
+    });
+
+    it('8xy4 - ADD Vx, Vy', () => {
         cpu.registerV[0] = 0x10;
         cpu.registerV[1] = 0x20;
         cpu._addVxVy(0, 1);
