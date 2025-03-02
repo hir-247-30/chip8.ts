10538e70eba1677a46054953ba16690d31fad24a
Hiroshi Hagiwara <mukakinkatsubou@gmail.com>
2025-03-02 18:58:58 +0900

    fix: webのrenderおかしい

--- src/webdisplay.ts
+++ src/webdisplay.ts
@@ -48,12 +48,14 @@ export class WebDisplay {
     }
 
     renderDisplay () {
-        this.#ctx.fillStyle = '#006400';
         for (let y = 0; y < DISPLAY_HEIGHT; y++) {
             for (let x = 0; x < DISPLAY_WIDTH; x++) {
                 if (this.#displayBuffer[y][x]) {
-                    this.#ctx.fillRect(x * 10, y * 10, 10, 10);
+                    this.#ctx.fillStyle = '#006400';
+                } else {
+                    this.#ctx.fillStyle = 'black';
                 }
+                this.#ctx.fillRect(x * 10, y * 10, 10, 10);
             }
         }
     }
