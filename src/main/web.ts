04e29cc90f7fc910b86e6ccd6fcc8ffe274cfff9
Hiroshi Hagiwara <mukakinkatsubou@gmail.com>
2025-03-07 23:11:07 +0900

    fix: - 一時停止ボタン - イベントトリガー修正

--- src/main/web.ts
+++ src/main/web.ts
@@ -9,13 +9,18 @@ let runnning = false;
 
 const sleep = (time: number) => new Promise((resolve) => setTimeout(resolve, time));
 
-document.getElementById('roms')!.addEventListener('click', async (event) => {
+document.getElementById('roms')!.addEventListener('change', async (event) => {
     const target = event.currentTarget as HTMLInputElement; // EventTarget だと解釈される
     const rom = target.value;
 
     let romBuffer: Buffer<ArrayBufferLike>;
     try {
         const response = await fetch(rom);
+        // エラー時にcatchしてくれないブラウザがある
+        if (!response.ok) {
+            throw new Error();
+        }
+
         const arrayBuffer = await response.arrayBuffer();
         // Bufferはブラウザだと使えない
         romBuffer =  new Uint8Array(arrayBuffer) as Buffer<ArrayBufferLike>;
@@ -35,6 +40,11 @@ document.getElementById('roms')!.addEventListener('click', async (event) => {
     cpu.readRom(romBuffer);
 
     runnning = true;
+    loop();
+});
+
+document.getElementById('pause')!.addEventListener('click', () => {
+    runnning = !runnning;
     loop();
 });
 
