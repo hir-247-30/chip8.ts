e05edf6a4155c16d37af82ffc3ce92e02f109542
Hiroshi Hagiwara <mukakinkatsubou@gmail.com>
2025-03-08 12:30:06 +0900

    fix: ブラウザ公開まわり

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -1,9 +1,9 @@
-import * as esbuild from "esbuild";
+import * as esbuild from 'esbuild';
 
 await esbuild.build({
     entryPoints: ['src/main/web.ts'],
     bundle     : true,
-    outfile    : 'docs/chip8.js',
+    outfile    : 'public/chip8.js',
     platform   : 'browser',
     format     : 'esm',
 }).catch(() => process.exit(1));
